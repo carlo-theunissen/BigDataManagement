635a68d8f24ce248b45c634be87206caba08c614
Twan van Maastricht <twanvm92@gmail.com>
2021-05-02 01:09:20 +0200

    Added jupyter notebook with exploratory data analysis of flight dataset

--- sparkflight/main.py
+++ sparkflight/main.py
@@ -6,10 +6,18 @@ from pyspark.sql import SparkSession
 
 from configparser import ConfigParser
 config = ConfigParser()
+# create your own config.ini in root of project folder to store project configurations
 config.read('../config.ini')
-path_to_csv = config.get('main', 'csv')
 
-# path_to_csv = "/mnt/c/Users/twanv/flight_data/airline.csv"
+data_is_clean = config.getboolean('main', 'preprocessing')
+
+if data_is_clean:
+    print("data is clean")
+    path_to_csv = config.get('main', 'clean_csv')
+
+else:
+    print("data is dirty")
+    path_to_csv = config.get('main', 'dirty_csv')
 
 
 logFile = path_to_csv  # Should be some file on your system
@@ -17,11 +25,25 @@ spark = SparkSession.builder \
     .config("spark.driver.memory", "15g") \
     .appName("SparkFlight").getOrCreate()
 
-logData = spark.read.text(logFile).cache()
+logData = spark.read.csv(logFile).cache()
 
-numAs = logData.filter(logData.value.contains('a')).count()
-numBs = logData.filter(logData.value.contains('b')).count()
+if data_is_clean:
+    # 
+    logData.head()
+else:
+    # result = logData.limit(10**3).
+    amount_rows = logData.count()
+    # > 120 million
+    print(amount_rows)
+    # logData.rdd.map(lambda row: ())
 
-print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+# result = logData.take(10)
 
+# print(result)
+
+# numAs = logData.filter(logData.value.contains('a')).count()
+# numBs = logData.filter(logData.value.contains('b')).count()
 spark.stop()
+
+# def unique_val_cols(row):
+#     row.
