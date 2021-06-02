b2b8bd3c137b25437e3d799eec3886f42862bc9b
ruben_heek <rheek@zig.nl>
2021-06-02 21:42:03 +0200

    fixes

--- main/hard.py
+++ main/hard.py
@@ -3,29 +3,29 @@ import time
 
 
 
-def get_flat_map(lhs_size, bv_candidate_FDs):
+def get_flat_map(lhs_size, candidate_FDs):
     if(lhs_size == 1):
-        return lambda row: [((i, row[fd[0]]), (row[fd[1]], True)) for i, fd in bv_candidate_FDs.value]
+        return lambda row: [((i, row[fd[0]]), (row[fd[1]], True)) for i, fd in candidate_FDs]
     if(lhs_size == 2):
-        return lambda row: [((i, row[fd[0]], row[fd[1]]), (row[fd[2]], True)) for i, fd in bv_candidate_FDs.value]
+        return lambda row: [((i, row[fd[0]], row[fd[1]]), (row[fd[2]], True)) for i, fd in candidate_FDs]
     if(lhs_size == 3):
-        return lambda row: [((i, row[fd[0]], row[fd[1]], row[fd[2]]), (row[fd[3]], True)) for i, fd in bv_candidate_FDs.value]
+        return lambda row: [((i, row[fd[0]], row[fd[1]], row[fd[2]]), (row[fd[3]], True)) for i, fd in candidate_FDs]
 
 
 
-def sample_FDs(dataframe, bv_candidate_FDs, lhs_size, sample_rate):
-    if(len(bv_candidate_FDs.value) == 0):
-        return bv_candidate_FDs
+def sample_FDs(dataframe, candidate_FDs, lhs_size, sample_rate):
+    if(len(candidate_FDs) == 0):
+        return candidate_FDs
     
     rdd = dataframe.rdd
     sample = rdd.sample(False, sample_rate)
-    mapped_FDs = sample.flatMap(get_flat_map(lhs_size, bv_candidate_FDs))
+    mapped_FDs = sample.flatMap(get_flat_map(lhs_size, candidate_FDs))
     grouped = mapped_FDs.reduceByKey(lambda x, y: y if(x == y) else (x[0], False))
     data_stripped = grouped.map(lambda x: (x[0][0], x[1][1]))
     grouped_by_fd = data_stripped.reduceByKey(lambda x, y: x and y)
     filtered = grouped_by_fd.filter(lambda x: x[1])
     bool_stripped = filtered.map(lambda x: x[0])
-    remaining_FDs = bool_stripped.map(lambda x: bv_candidate_FDs.value[x])
+    remaining_FDs = bool_stripped.map(lambda x: candidate_FDs[x])
     
     return remaining_FDs.collect()
 
@@ -33,27 +33,27 @@ validate_FDs = lambda dataframe, bv_candidate_FDs, lhs_size: sample_FDs(datafram
 
 
 
+# TODO: use broadcasting?
 # TODO: use batching?
-def find_FDs(output_file, spark, dataframe, lhs_sizes, sample_rates, colLimit = -1):
-    col_names = utils.get_all_cols(dataframe)
-    col_names =  col_names if colLimit == -1 else col_names[:colLimit]
+def find_FDs(output_file, spark, dataframe, lhs_sizes, sample_rates, col_limit = -1):
+    col_names = utils.get_col_names(dataframe)
+    col_names =  col_names if col_limit == -1 else col_names[:col_limit]
 
     found_FDs = []
     for lhs_size in lhs_sizes:
         candidate_FDs = utils.generate_deps(col_names, col_names, lhs_size, found_FDs)
-        bv_candidate_FDs = spark.sparkContext.broadcast(candidate_FDs)
 
         for sample_rate in sample_rates:
             output_file.write(f'Running sampling rate {sample_rate} over {len(candidate_FDs)} candidate FDs\n')
             tic = time.perf_counter()
 
             # find and keep only the remaining candidate_FDs
-            candidate_FDs = sample_FDs(dataframe, bv_candidate_FDs, lhs_size, sample_rate)
+            candidate_FDs = sample_FDs(dataframe, candidate_FDs, lhs_size, sample_rate)
 
             toc = time.perf_counter()
             output_file.write(f'Sampling took {toc - tic :0.4f} seconds\n')
         
-        validated_FDs = validate_FDs(dataframe, bv_candidate_FDs, lhs_size)
+        validated_FDs = validate_FDs(dataframe, candidate_FDs, lhs_size)
         found_FDs.append(validated_FDs)
 
     return found_FDs
